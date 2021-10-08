04571940b381b2d634cc16e7fb75a51242ef5812
takata142 <82187725+takata142@users.noreply.github.com>
2021-10-08 11:38:17 +0900

    add

--- index.js
+++ index.js
@@ -13,10 +13,39 @@ app.use(express.urlencoded({extended:true}))　
 app.post('/api',(req,res)=> getUserInfo(req,res))
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
 
+const LineNotifyConfig ={
+    client_id: `${process.env.LOGIN_CHANNEL_ID}`
+}
 
 const getUserInfo = (req,res) => {
-    const data = req.body;
-    //console.log('id_token:',data.id_token);
+    const idToken = req.body;
+
+    function get_response(){
+
+        let headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+    
+        let dataString = 'id_token=' + idToken.id_token;
+        dataString += '&client_id=' + LineNotifyConfig.client_id;
+    
+        let options ={
+            url:'https://api.line.me/oauth2/v2.1/verify',
+            method: 'POST',
+            headers: headers,
+            body: dataString
+        }
+    
+        function callback(error,response,body){
+            if(response.statusCode == 200){
+                console.log(body,"200 OK");
+            }else{
+                console.log("error")
+            }
+        }
+        request(options,callback)
+    }
+    get_response()
    }
 
 // function getUserInfo(req,res){
@@ -24,13 +53,8 @@ const getUserInfo = (req,res) => {
 //     console.log(data.id_token)
 // }
 
-getUserInfo(req.body)
 
-const LineNotifyConfig ={
-    client_id: `${process.env.LOGIN_CHANNEL_ID}`
-}
 
-// console.log(LineNotifyConfig.client_id)
 
 // function get_response(req,res){
 
@@ -57,11 +81,6 @@ const LineNotifyConfig ={
 //             console.log("error")
 //         }
 //     }
-
-
 //     request(options,callback)
-
 // }
-
-
 // get_response()
