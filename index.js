893e9e7f600646573837f72e80636d8258344fa4
takata142 <82187725+takata142@users.noreply.github.com>
2021-10-08 11:02:50 +0900

    add

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('public'))
 app.use(express.json()) 
 app.use(express.urlencoded({extended:true}))　
-app.post('/api',(req,res)=> getUserInfo(req,res))
+app.post('/api',(req,res)=> get_response(req,res))
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
 
 
@@ -19,44 +19,45 @@ app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
 //     console.log('id_token:',data.id_token);
 //    }
 
-function getUserInfo(req,res){
-    const data = req.body;
-    console.log(data.id_token)
-}
-
-// function get_response(req,res){
+// function getUserInfo(req,res){
 //     const data = req.body;
 //     console.log(data.id_token)
 // }
 
-// get_response()
+const LineNotifyConfig ={
+    client_id: '1656488947'
+}
 
-// function get_response(req,res) {
-//   const data = req.body;
-//   var headers = {
-//     'Content-Type': 'application/x-www-form-urlencoded'
-//   };
-// //Line 指定のやり取り
-//   var postData = `id_token=${data.id_token}&client_id=${process.env.LOGIN_CHANNEL_ID}`;
-//   console.log(data.id_token)
+function get_response(req,res){
 
-//   var options = {
-//     url: 'https://notify-bot.line.me/oauth/token',
-//     method: 'POST',
-//     headers: headers,
-//     body: postData
-//   };
+    const idToken = req.body;
 
-//   function callback(error, response) {
-//     if (response.statusCode == 200) {
-//       //push_to_db(body, data)
-//       console.log("200 OK")
-//     } else {
-//       console.error('error',error)
-//     }
-//   }
-//     //nodejs module,callback
-//   return request(options, callback);
+    let headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    };
 
-// }
-// console.log(get_response())
+    let dataString = 'id_token=' + idToken.id_token;
+    dataString += '&client_id=' + LineNotifyConfig.client_id;
+
+    let options ={
+        url:'https://api.line.me/oauth2/v2.1/verify',
+        method: 'POST',
+        headers: headers,
+        body: dataString
+    }
+
+    function callback(error,response,body){
+        if(response.statusCode == 200){
+            console.log(body,"200 OK");
+        }else{
+            console.log(response)
+        }
+    }
+
+
+    request(options,callback)
+
+}
+
+
+get_response()
